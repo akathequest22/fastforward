07274bc0f63b6814fa75efc1f542636e0b14b778
akathequest22 <htoohtetaung.officially@gmail.com>
2025-10-10 18:44:46 +0700

    ddd

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/fastforward/', // ✅ သင့် repo name က "fastforward"
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
-})
+    base: '/fastforward/',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets'
+    }
+});
